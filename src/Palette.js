52f775a7c93741488c40f94a373f5959f30485bf
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2020-12-08 12:34:13 -0500

    Add SingleColorPalette and Link

--- src/Palette.js
+++ src/Palette.js
@@ -20,10 +20,16 @@ class Palette extends Component {
   }
 
   render() {
-    const { colors, paletteName, emoji } = this.props.palette
+    const { colors, paletteName, emoji, id } = this.props.palette
     const { level, format } = this.state
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color[format]} name={color.name} key={color.id} />
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        id={color.id}
+        paletteId={id}
+        key={color.id}
+      />
     ))
     return (
       <div className='Palette'>
