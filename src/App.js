ae3317333712bd16e1492cefde52264748d43747
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2020-12-15 12:02:12 -0500

    Save palettes to Local Storage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { generatePalette } from './colorHelpers.js'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { palettes: seedColors }
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'))
+    this.state = { palettes: savedPalettes || seedColors }
     this.findPalette = this.findPalette.bind(this)
     this.savePalette = this.savePalette.bind(this)
   }
@@ -21,7 +22,14 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] })
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    )
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes))
   }
 
   render() {
