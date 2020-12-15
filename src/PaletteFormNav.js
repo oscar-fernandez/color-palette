ae87696274d984acc4972bff4d5a61f93710349e
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2020-12-15 11:55:03 -0500

    Style NewPaletteFormStyles and PaletteFormNavStyles

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,7 +8,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
+import EditIcon from '@material-ui/icons/Edit'
 import Button from '@material-ui/core/button'
 import styles from './styles/PaletteFormNavStyles'
 
@@ -50,9 +50,11 @@ class PaletteFormNav extends Component {
               color='inherit'
               aria-label='Open drawer'
               onClick={this.props.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
+              className={classNames(classes.menuButton, {
+                [classes.hide]: open,
+              })}
             >
-              <MenuIcon />
+              <EditIcon />
             </IconButton>
             <Typography variant='h6' color='inherit' noWrap>
               Create a Palette
